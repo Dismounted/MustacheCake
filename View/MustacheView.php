caf03e9a19132820f3b5f2687cb235a53f557479
Hanson Wong <dismounted@server.fake>
2013-07-14 02:20:51 +1000

    Add custom view class skeleton.

---
+++ View/MustacheView.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * MustacheCake
+ *
+ * A Mustache implementation for CakePHP.
+ *
+ * @copyright     Copyright (c) Hanson Wong
+ * @link          https://github.com/Dismounted/MustacheCake
+ * @package       MustacheCake.View
+ * @license       Simplified BSD License (https://github.com/Dismounted/MustacheCake/blob/master/LICENSE)
+ */
+
+App::uses('View', 'View');
+
+/**
+ * Mustache view class.
+ *
+ * This extends the default View class to provide Mustache rendering functionality.
+ *
+ * @package       MustacheCake.View
+ */
+class MustacheView extends View {
+
+}
