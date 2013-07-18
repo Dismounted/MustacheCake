f1598739d6c9b54de97907a9ed0f14321bbffb04
Hanson Wong <dismounted@server.fake>
2013-07-18 12:34:07 +1000

    Fix T_CONSTANT_ENCAPSED_STRING.

--- View/MustacheView.php
+++ View/MustacheView.php
@@ -52,7 +52,7 @@ class MustacheView extends View {
 		parent::__construct();
 
 		if (class_exists('Mustache_Autoloader', false) === false) {
-			App::import('Vendor', 'MustacheAutoloader', array('file' => 'Mustache' . DS . 'src' . DS 'Mustache' . DS . 'Autoloader.php'));
+			App::import('Vendor', 'MustacheAutoloader', array('file' => 'Mustache' . DS . 'src' . DS . 'Mustache' . DS . 'Autoloader.php'));
 			Mustache_Autoloader::register();
 		}
 
