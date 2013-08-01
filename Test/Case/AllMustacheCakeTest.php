4fe8225c8b194bc2bacbf0a9a6b409938ee4834a
Hanson Wong <dismounted@server.fake>
2013-08-01 10:41:41 +1000

    Add skeleton test suite.

---
+++ Test/Case/AllMustacheCakeTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * MustacheCake
+ *
+ * A Mustache implementation for CakePHP.
+ *
+ * @copyright     Copyright (c) Hanson Wong
+ * @link          https://github.com/Dismounted/MustacheCake
+ * @package       MustacheCake.Test.Case
+ * @license       https://github.com/Dismounted/MustacheCake/blob/master/LICENSE Simplified BSD License
+ */
+
+/**
+ * MustacheCake test suite.
+ *
+ * This suite runs all the available tests for MustacheCake.
+ *
+ * @package       MustacheCake.Test.Case
+ */
+class AllMustacheCakeTest extends CakeTestSuite {
+	
+}
